d172bf4a7e33da57a07d47a3ba6b1db8858fbed9
Gilang Ramadhan Nugraha <gilangramadhannugraha@gmail.com>
2020-09-20 15:18:27 +0700

    done initialization get it

--- lib/injection/injection.dart
+++ lib/injection/injection.dart
@@ -1,1 +1,34 @@
-class Injection {}
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
+import 'package:movieapp/data/core/movie_api_client.dart';
+import 'package:movieapp/data/data_sources/remote/movies_data_source.dart';
+import 'package:movieapp/data/repositories/movie_repository_implement.dart';
+import 'package:movieapp/domain/repositories/movies_repository.dart';
+import 'package:movieapp/domain/usecases/get_coming_soon.dart';
+import 'package:movieapp/domain/usecases/get_playing_now.dart';
+import 'package:movieapp/domain/usecases/get_popular.dart';
+import 'package:movieapp/domain/usecases/get_trending.dart';
+
+final getItInstance = GetIt.I;
+
+Future init() async {
+  getItInstance.registerLazySingleton<Client>(() => Client());
+  getItInstance.registerLazySingleton<MovieApiClient>(
+      () => MovieApiClient(getItInstance()));
+
+  getItInstance.registerLazySingleton<MoviesDataSource>(
+      () => MovieDataSourceImplement(getItInstance()));
+
+  getItInstance.registerLazySingleton<MoviesRepository>(
+      () => MoviesRepositoryImplement(getItInstance()));
+
+  //DI Usecases
+  getItInstance
+      .registerLazySingleton<GetTrending>(() => GetTrending(getItInstance()));
+  getItInstance
+      .registerLazySingleton<GetPopular>(() => GetPopular(getItInstance()));
+  getItInstance.registerLazySingleton<GetComingSoon>(
+      () => GetComingSoon(getItInstance()));
+  getItInstance.registerLazySingleton<GetPlayingNow>(
+      () => GetPlayingNow(getItInstance()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,17 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:movieapp/data/data_sources/remote/movies_data_source.dart';
-import 'package:movieapp/data/repositories/movie_repository_implement.dart';
+import 'package:pedantic/pedantic.dart';
 import 'package:movieapp/domain/entities/movies_entity.dart';
 import 'package:movieapp/domain/entities/movies_error.dart';
 import 'package:movieapp/domain/entities/no_params.dart';
-import 'package:movieapp/domain/repositories/movies_repository.dart';
 import 'package:movieapp/domain/usecases/get_trending.dart';
 import 'package:movieapp/presentation/views/appku.dart';
-
-import 'data/core/movie_api_client.dart';
+import 'injection/injection.dart ' as getIt;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  MovieApiClient apiClient = MovieApiClient(Client());
-  MoviesDataSource remoteDataSource = MovieDataSourceImplement(apiClient);
-  MoviesRepository moviesRepository =
-      MoviesRepositoryImplement(remoteDataSource);
-  GetTrending getTrending = GetTrending(moviesRepository);
+  unawaited(getIt.init());
+  GetTrending getTrending = getIt.getItInstance<GetTrending>();
   final Either<MoviesError, List<MoviesEntity>> eitherResp =
       await getTrending(NoParams());
   eitherResp.fold((l) => print(l), (r) => print(r));
