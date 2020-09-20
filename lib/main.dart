f33c70ca45d99a41c16c9875a62bfe00dc744e9a
Gilang Ramadhan Nugraha <gilangramadhannugraha@gmail.com>
2020-09-20 14:40:04 +0700

    add library getit for depedency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:movieapp/data/data_sources/remote/movies_data_source.dart';
 import 'package:movieapp/data/repositories/movie_repository_implement.dart';
+import 'package:movieapp/domain/entities/movies_entity.dart';
+import 'package:movieapp/domain/entities/movies_error.dart';
+import 'package:movieapp/domain/entities/no_params.dart';
 import 'package:movieapp/domain/repositories/movies_repository.dart';
 import 'package:movieapp/domain/usecases/get_trending.dart';
 import 'package:movieapp/presentation/views/appku.dart';
@@ -15,7 +19,8 @@ void main() async {
   MoviesRepository moviesRepository =
       MoviesRepositoryImplement(remoteDataSource);
   GetTrending getTrending = GetTrending(moviesRepository);
-  getTrending();
-
+  final Either<MoviesError, List<MoviesEntity>> eitherResp =
+      await getTrending(NoParams());
+  eitherResp.fold((l) => print(l), (r) => print(r));
   runApp(Appku());
 }
